968f039866576814c00797c98984e8e21189c786
artem <artialbk@gmail.com>
2024-10-16 06:24:35 +0300

    dsf

---
+++ script.js
@@ -0,0 +1,42 @@
+// let userInput= +prompt("enter first number")
+// let userInput2= +prompt("enter second number")
+
+// function sumOfNumbers(userInput,userInput2){
+//     let sum=userInput+userInput2
+//     return sum
+// }
+// alert(sumOfNumbers(userInput,userInput2))
+
+
+
+// function readNumber(){
+//     let num;
+//     do{
+//         num = prompt("enter the number", 0);
+//     } while( !isFinite(num) );
+
+//     if(num===null || num=== ""){
+//         return null;
+//     }
+//     return +num;    
+// }    
+
+// alert(`number: ${readNumber()}`);
+    
+
+    
+// function random(min, max) {
+//     return min + Math.random() * (max - min);
+//   }
+
+//   alert( random(1,7) );
+
+
+
+// function randomInteger(min, max) {
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+//   }
+  
+//   alert(randomInteger(1, 3));
+
